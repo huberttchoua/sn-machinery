9a33cd20b2d5f666231677d7e6ed5edf575eb3ff
Karuranga Hubert <myfullmovis@gmail.com>
2026-06-10 22:25:47 +0200

    Commit

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import path from 'path';
+import fs from 'fs';
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+const rootEnvPath = path.join(__dirname, '../../.env');
+const serverEnvPath = path.join(__dirname, '../.env');
+
+if (fs.existsSync(serverEnvPath)) {
+    dotenv.config({ path: serverEnvPath });
+} else if (fs.existsSync(rootEnvPath)) {
+    dotenv.config({ path: rootEnvPath });
+} else {
+    dotenv.config();
+}
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
